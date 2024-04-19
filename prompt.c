46b8e9fa77f2a4f26911b0b42d10af305fc1e23c
Xavier308 <xavvis308@gmail.com>
2024-04-19 13:25:55 -0400

    modification to the simple shell so it detects when it's being used in a non-interactive mode

--- prompt.c
+++ prompt.c
@@ -6,11 +6,15 @@
  * Description: This function prints the custom prompt for the shell, which is
  * "#supershell$ ". It ensures the prompt is displayed immediately by flushing
  * the standard output buffer.
+ * The prompt is not displayed if the input is not
+ * from a terminal (i.e., in non-interactive mode).
  */
 void display_prompt(void)
 {
-	printf("#supershell$ "); /* Print the prompt text */
-
-	/* Flush the output buffer to ensure prompt shows immediately */
-	fflush(stdout);
+	if (isatty(STDIN_FILENO)) /*  Check if the stdin is a terminal */
+	{
+		printf("#supershell$ "); /* Print the prompt text */
+		/* Flush the output buffer to ensure prompt shows immediately */
+		fflush(stdout);
+	}
 }
